2ff0cedddfd47c1f8ef7b330ff919771e4300e5c
Juliy <tima1002100@mail.ru>
2022-12-01 18:38:33 +0300

    added Hw01

---
+++ HomeworkC#01/Program.cs
@@ -0,0 +1,22 @@
+﻿Задача 1: Напишите программу, 
+которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber < secondNumber)
+{
+    Console.Write(" min = "); 
+    Console.Write(firstNumber);
+    Console.Write(" max = ");
+    Console.WriteLine(secondNumber);
+}
+else
+{
+    Console.Write(" max = ");
+    Console.Write(firstNumber);
+    Console.Write(" min = ");
+    Console.WriteLine(secondNumber);
+}
