fb968cc255136de93e5d48d83ad2c79137dec342
Juliy <tima1002100@mail.ru>
2022-12-01 18:39:57 +0300

    added Hw04

---
+++ HomeworkC#04/Program.cs
@@ -0,0 +1,11 @@
+﻿Задача 4: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int firstNumber = 2;
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+while (firstNumber <= secondNumber)
+{
+    Console.Write (firstNumber + ",");
+    firstNumber += 2;
+}
