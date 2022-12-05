b300ca4579c0d2584ae9dcaa715362888ee85323
Juliy <tima1002100@mail.ru>
2022-12-05 10:24:19 +0300

    added Hw05

---
+++ HomeworkC#05/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.Write("Исходное число: ");
+// int number = new Random().Next(100, 1000);
+// int firstDigit = number / 10;
+// Console.WriteLine(number);
+// Console.WriteLine("Вторая цифра этого числа: " + firstDigit % 10);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 10000 && number < 100000) Console.WriteLine(number / 100 % 10);
+// if (number > 1000 && number < 10000) Console.WriteLine(number / 10 % 10);
+// if (number > 100 && number < 1000) Console.WriteLine(number % 10);
+// if (number < 100) Console.WriteLine("Третьей цифры нет");
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("Введите цифру дня недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number == 1 | number == 2 | number == 3 | number ==4 | number == 5) Console.Write (number + " - Не выходной");
+// if (number == 6 | number == 7) Console.Write ( number + " - Выходной");
