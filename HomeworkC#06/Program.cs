cbb1b522b6fd31377923bcf042d4d83e81cd6b2b
Juliy <tima1002100@mail.ru>
2022-12-05 16:05:06 +0300

    added Hw06

---
+++ HomeworkC#06/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// Console.Write("Исходное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number >= 10000)
+// if (number / 1 % 10 == number / 10000 % 10);
+// if (number / 10 % 10 == number / 1000 % 10); 
+// if (number / 100 % 10 == number / 100 % 10); 
+// if (number / 1000 % 10 == number / 10 % 10); 
+// if (number / 10000 % 10 == number / 1 % 10) Console.Write(number + " - Является полидромом"); 
+// else Console.WriteLine(number + " - Не является полиндромом");
+
+
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+// Console.Write("Введите координату х1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z2: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// Console.WriteLine("Расстояние между 2 точками: " + Math.Round(d,2));
+
+
+// Задача 23 Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.Write("Введите число N: ");
+// int finish = Convert.ToInt32(Console.ReadLine());
+// for (int start = 1; start <= finish;  start++) 
+// Console.Write(Math.Pow(start,3) + ",");
